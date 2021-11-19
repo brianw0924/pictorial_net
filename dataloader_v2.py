1a75617668ea1fee9f7b5554d10ee6fde504eea7
brian <brianw0924@gmail.com>
2021-11-19 13:36:19 +0800

    option: cross target or mixed target

--- dataloader_v2.py
+++ dataloader_v2.py
@@ -9,21 +9,14 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 from PIL import Image
 
-def tfm(image):
-    a = random.randint(0,200)
-    b = random.randint(0,200)
-    c = random.randint(0,200)
-    d = random.randint(0,200)
-    padding = (a,b,c,d)
-    transform = transforms.Compose([
-        transforms.Pad(padding, fill=0,padding_mode="constant"),
-        transforms.Resize((144,192)),
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-    ])
-    return transform(image)
 
-class TEyeDDataset(data.Dataset):
+tfm = transforms.Compose([
+    transforms.Resize((144,192)),
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+])
+
+class TEyeDDataset_mixed(data.Dataset):
     '''
     dataset_dir
         |-image
@@ -35,7 +28,7 @@ class TEyeDDataset(data.Dataset):
         
         self.image_path = sorted(glob.glob(os.path.join(dataset_dir,"image","*")))
         self.gaze_path = os.path.join(dataset_dir,"gaze","gaze.txt")
-        self.landmark_path = os.path.join(dataset_dir,"landmark","landmark.txt")
+        self.landmark_path = os.path.join(dataset_dir,"landmark","iris_landmark.txt")
         self.gaze = []
         self.landmark = []
         with open(self.gaze_path) as f:
@@ -47,7 +40,9 @@ class TEyeDDataset(data.Dataset):
             for line in f.readlines():
                 line = line.split(';')
                 self.landmark.append(torch.tensor([float(l) for l in line[2:-1]]))
-        
+        print(len(self.gaze))
+        print(len(self.landmark))
+        print(len(self.image_path))
         assert(len(self.gaze) == len(self.landmark) == len(self.image_path))
 
     def __getitem__(self, index):
@@ -57,30 +52,76 @@ class TEyeDDataset(data.Dataset):
     def __len__(self):
         return len(self.image_path)
 
-def get_loader_TEyeD(dataset_dir, batch_size, num_workers, use_gpu):
+class TEyeDDataset_cross(data.Dataset):
+    '''
+    dataset_dir
+        |-image
+        |-gaze
+        |-landmark
+        |-segmentation
+    '''
+    def __init__(self, dataset_dir, ratio, val):
+        self.image_path = sorted(glob.glob(os.path.join(dataset_dir,"image","*")))
+        self.gaze_path = os.path.join(dataset_dir,"gaze","gaze.txt")
+        self.landmark_path = os.path.join(dataset_dir,"landmark","iris_landmark.txt")
+        self.gaze = []
+        self.landmark = []
+        with open(self.gaze_path) as f:
+            for line in f.readlines():
+                line = line.split(';')
+                self.gaze.append(torch.tensor([float(g) for g in line[1:-1]]))
+        
+        with open(self.landmark_path) as f:
+            for line in f.readlines():
+                line = line.split(';')
+                self.landmark.append(torch.tensor([float(l) for l in line[2:-1]]))
+       
+        assert(len(self.gaze) == len(self.landmark) == len(self.image_path))
 
-    # train: 0.7 ; val: 0.3
-    ratio = 0.7
+        split = int(len(self.image_path)*ratio)
+        if val:
+            self.image_path = self.image_path[split:]
+            self.gaze = self.gaze[split:]
+            self.landmark = self.landmark[split:]
+        else:
+            self.image_path = self.image_path[:split]
+            self.gaze = self.gaze[:split]
+            self.landmark = self.landmark[:split]
 
-    train_dataset = TEyeDDataset(dataset_dir)
-    split_len = int(len(train_dataset) * ratio)
-    train_dataset, val_dataset = data.random_split(train_dataset, [split_len, len(train_dataset) - split_len])
+    def __getitem__(self, index):
+        image = Image.open(self.image_path[index])
+        return tfm(image),  self.gaze[index]
+
+    def __len__(self):
+        return len(self.image_path)
+
+
+def get_loader_TEyeD(args):
+
+    if args.cross_target:
+        train_dataset = TEyeDDataset_cross(dataset_dir=args.dataset, ratio=args.ratio, val=False)
+        val_dataset  = TEyeDDataset_cross(dataset_dir=args.dataset, ratio=args.ratio, val=True)
+    else:
+        train_dataset = TEyeDDataset_mixed(dataset_dir=args.dataset)
+        split_len = int(len(train_dataset) * args.ratio)
+        train_dataset, val_dataset = data.random_split(train_dataset, [split_len, len(train_dataset) - split_len])
+    
     print(f'train data len: {len(train_dataset)} | validation data len: {len(val_dataset)}')
 
     train_loader = data.DataLoader(
         train_dataset,
-        batch_size=batch_size,
+        batch_size=args.batch_size,
         shuffle=True,
-        num_workers=num_workers,
-        pin_memory=use_gpu,
+        num_workers=args.num_workers,
+        pin_memory=True,
         drop_last=True,
     )
     val_loader = data.DataLoader(
         val_dataset,
-        batch_size=1,
-        num_workers=num_workers,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
         shuffle=False,
-        pin_memory=use_gpu,
-        drop_last=False,
+        pin_memory=True,
+        drop_last=True,
     )
     return train_loader, val_loader
--- main.py
+++ main.py
@@ -21,20 +21,33 @@ from tqdm import tqdm
 
 from utils import Set_seed, vec2angle, pitch_loss, yaw_loss, plot_curve, update_log
 from dataloader import TEyeDDataset
-from dataloader import get_loader_TEyeD
+from dataloader_v2 import get_loader_TEyeD
 from models.mynet import Mynet 
+from models.pictorial_net import Model
+
+'''
+Before Training:
+
+Remember to confirm:
+0. model ?
+1. args.outdir ?
+2. args.cross_target ?
+'''
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default="/home/brianw0924/hdd/processed_data")
-    parser.add_argument('--outdir', type=str, default='./result/UNet_vgg16bn')
+    parser.add_argument('--dataset', type=str, default="/home/brianw0924/hdd/TEyeD")
+    parser.add_argument('--outdir', type=str, default='./result/cross_target/UNet16_linear1024')
+    parser.add_argument('--cross_target', action="store_true")
+    parser.add_argument('--ratio', type=float, default=0.7)
     parser.add_argument('--seed', type=int, default=17)
     parser.add_argument('--num_workers', type=int, default=8)
     parser.add_argument('--epochs', type=int, default=20)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--base_lr', type=float, default=1e-4)
     parser.add_argument('--weight_decay', type=float, default=1e-3)
-    parser.add_argument('--milestones', type=list, default=[6,15])
+    parser.add_argument('--milestones', type=list, default=[5,15])
     parser.add_argument('--lr_decay', type=float, default=0.1)
 
     args = parser.parse_args()
@@ -43,86 +56,6 @@ def parse_args():
 
     return args
 
-def train_dynamic_loading(args, epoch, model, optimizer, criterion, dataset_dir, device):
-
-    tqdm.write('Train {}'.format(epoch))
-
-    all_path = glob.glob(os.path.join(dataset_dir,"*"))[:-1]
-
-    model.train()
-
-    loss_list = []
-    pitch_loss_list = []
-    yaw_loss_list = []
-
-    for num_file, file in enumerate(tqdm(all_path)):
-        train_dataset = TEyeDDataset(file)
-        # tqdm.write(f'Training on : {file} | Image amount: {len(train_dataset)}')
-        if len(train_dataset) == 0:
-            continue
-
-        train_loader = torch.utils.data.DataLoader(
-            train_dataset,
-            batch_size=args.batch_size,
-            shuffle=True,
-            num_workers=args.num_workers,
-            pin_memory=True,
-            drop_last=True,
-        )
-
-        loss_list_inner = []
-        pitch_loss_list_inner = []
-        yaw_loss_list_inner = []
-
-        # Inner loop #
-
-        for step, (images, gazes) in enumerate(train_loader):
-
-            optimizer.zero_grad()
-            outputs = model(images.to(device))
-            loss = criterion(outputs, vec2angle(gazes).to(device))
-            loss.backward()
-            optimizer.step()
-
-            p_loss = pitch_loss(outputs, vec2angle(gazes).to(device))
-            y_loss = yaw_loss(outputs, vec2angle(gazes).to(device))
-        
-            
-            loss_list_inner.append(loss.item())
-            pitch_loss_list_inner.append(p_loss.item())
-            yaw_loss_list_inner.append(y_loss.item())
-
-        # Inner loop #
-
-        loss_avg = sum(loss_list_inner) / len(loss_list_inner)
-        p_loss_avg = sum(pitch_loss_list_inner) / len(pitch_loss_list_inner)
-        y_loss_avg = sum(yaw_loss_list_inner) / len(yaw_loss_list_inner)
-
-        loss_list.append(loss_avg)
-        pitch_loss_list.append(p_loss_avg)
-        yaw_loss_list.append(y_loss_avg)
-
-        tqdm.write('Train | Epoch {} Step {}/{} | '
-                    'Loss {:.4f} | '
-                    'Pitch loss {:2f} | '
-                    'Yaw loss {:2f}'.format(
-                        epoch,
-                        num_file+1,
-                        len(all_path),
-                        loss_avg,
-                        p_loss_avg,
-                        y_loss_avg
-                    ))
-        
-        del train_loader, train_dataset, loss_list_inner
-
-    loss_avg = sum(loss_list) / len(loss_list)
-    pitch_loss_avg = sum(pitch_loss_list) / len(pitch_loss_list)
-    yaw_loss_avg = sum(yaw_loss_list) / len(yaw_loss_list)
-
-    return loss_avg, pitch_loss_avg, yaw_loss_avg
-
-
 def train(args, epoch, model, optimizer, criterion, train_loader, device, log):
     model.train()
 
@@ -130,10 +63,14 @@ def train(args, epoch, model, optimizer, criterion, train_loader, device, log):
     pitch_loss_list = []
     yaw_loss_list = []
 
+    count = 0
     for step, (images, gazes) in enumerate(tqdm(train_loader)):
+        count+=gazes.shape[0]
+
         optimizer.zero_grad()
         outputs = model(images.to(device))
         loss = criterion(outputs, vec2angle(gazes).to(device))
+
         loss.backward()
         optimizer.step()
 
@@ -144,15 +81,15 @@ def train(args, epoch, model, optimizer, criterion, train_loader, device, log):
         pitch_loss_list.append(p_loss.item())
         yaw_loss_list.append(y_loss.item())
 
-    loss_avg = sum(loss_list) / len(loss_list)
-    pitch_loss_avg = sum(pitch_loss_list) / len(pitch_loss_list)
-    yaw_loss_avg = sum(yaw_loss_list) / len(yaw_loss_list)
+    loss_avg = sum(loss_list) / count
+    pitch_loss_avg = sum(pitch_loss_list) / count
+    yaw_loss_avg = sum(yaw_loss_list) / count
 
     tqdm.write( 'Train | '
                 'Epoch {} | '
                 'Loss {:.4f} | '
-                'Pitch loss {:2f} | '
-                'Yaw loss {:2f}'.format(
+                'Pitch error (MAE) {:2f} | '
+                'Yaw error (MAE) {:2f}'.format(
                     epoch,
                     loss_avg,
                     pitch_loss_avg,
@@ -163,15 +100,16 @@ def train(args, epoch, model, optimizer, criterion, train_loader, device, log):
 
     return loss_avg, pitch_loss_avg, yaw_loss_avg
 
-def test(args, epoch, model, criterion, test_loader, device, log):
+def validation(args, epoch, model, criterion, val_loader, device, log):
     model.eval()
 
     loss_list = []
     pitch_loss_list = []
     yaw_loss_list = []
 
-    for step, (images, gazes) in enumerate(tqdm(test_loader)):
-
+    count = 0
+    for step, (images, gazes) in enumerate(tqdm(val_loader)):
+        count+=gazes.shape[0]
         with torch.no_grad():
             outputs = model(images.to(device))
 
@@ -184,22 +122,22 @@ def test(args, epoch, model, criterion, test_loader, device, log):
         yaw_loss_list.append(y_loss.item())
 
 
-    loss_avg = sum(loss_list) / len(loss_list)
-    pitch_loss_avg = sum(pitch_loss_list) / len(pitch_loss_list)
-    yaw_loss_avg = sum(yaw_loss_list) / len(yaw_loss_list)
+    loss_avg = sum(loss_list) / count
+    pitch_loss_avg = sum(pitch_loss_list) / count
+    yaw_loss_avg = sum(yaw_loss_list) / count
     
-    tqdm.write( 'Test | '
+    tqdm.write( 'Validation  | '
                 'Epoch {} | '
                 'Loss {:.4f} | '
-                'Pitch loss {:.2f} | '
-                'Yaw loss {:2f}'.format(
+                'Pitch error (MAE) {:.2f} | '
+                'Yaw error (MAE) {:2f}'.format(
                     epoch,
                     loss_avg,
                     pitch_loss_avg,
                     yaw_loss_avg
                 ))
 
-    update_log(args, log, "Test", epoch, loss_avg, pitch_loss_avg, yaw_loss_avg)
+    update_log(args, log, "Validation", epoch, loss_avg, pitch_loss_avg, yaw_loss_avg)
 
     return loss_avg, pitch_loss_avg, yaw_loss_avg
 
@@ -212,7 +150,7 @@ def main():
     # TRAIN LOG
     log = {
         "Train":{},
-        "Test":{}
+        "Validation":{}
     }
 
     # ARGUMENTS
@@ -231,14 +169,14 @@ def main():
 
     # DATASET (TEyeD)
     print("Preparing data ...")
-    train_loader, test_loader = get_loader_TEyeD(
-        args.dataset, args.batch_size, args.num_workers, True)
+    train_loader, val_loader = get_loader_TEyeD(args)
 
     # MODEL & LOSS
     model = Mynet()
     model.to(device)
     # print(model)
-    criterion = nn.MSELoss(reduction='mean') # actually, default is already 'mean'
+    # torch.save(model.state_dict(), os.path.join(args.outdir, f'model_state.pth'))
+    criterion = nn.MSELoss(reduction='sum')
 
     # OPTIMIZER
     optimizer = torch.optim.Adam(
@@ -248,24 +186,23 @@ def main():
 
     # PLOTTING
     plot_train_loss, plot_train_pitch_loss, plot_train_yaw_loss = [], [], []
-    plot_test_loss, plot_test_pitch_loss, plot_test_yaw_loss = [], [], []
+    plot_val_loss, plot_val_pitch_loss, plot_val_yaw_loss = [], [], []
 
     # TRAINING
-    test(args, 0, model, criterion, test_loader, device, log) # first testing before training
+    validation(args, 0, model, criterion, val_loader, device, log) # first validationing before training
+
+    error = 1e10
     for epoch in range(1, args.epochs + 1):
 
         # Training
         train_loss, train_pitch_loss, train_yaw_loss = train(
             args, epoch, model, optimizer, criterion, train_loader, device, log
         )
-        # train_loss, train_pitch_loss, train_yaw_loss = train_dynamic_loading(
-        #     args, epoch, model, optimizer, criterion, args.dataset, device
-        # )
         scheduler.step()
 
         # Validation
-        test_loss, test_pitch_loss, test_yaw_loss = test(
-            args, epoch, model, criterion, test_loader, device, log
+        val_loss, val_pitch_loss, val_yaw_loss = validation(
+            args, epoch, model, criterion, val_loader, device, log
         )
 
         # Append loss for plotting
@@ -273,19 +210,22 @@ def main():
         plot_train_pitch_loss.append(train_pitch_loss)
         plot_train_yaw_loss.append(train_yaw_loss)
 
-        plot_test_loss.append(test_loss)
-        plot_test_pitch_loss.append(test_pitch_loss)
-        plot_test_yaw_loss.append(test_yaw_loss)
+        plot_val_loss.append(val_loss)
+        plot_val_pitch_loss.append(val_pitch_loss)
+        plot_val_yaw_loss.append(val_yaw_loss)
 
         # Save state
-        state = OrderedDict([
-            ('state_dict', model.state_dict()),
-            ('optimizer', optimizer.state_dict()),
-        ])
-        torch.save(state, os.path.join(args.outdir, f'model_state_epoch{epoch}.pth'))
+        if val_loss < error:
+            tqdm.write(f"Save model at epoch {epoch}")
+            state = OrderedDict([
+                ('state_dict', model.state_dict()),
+                # ('optimizer', optimizer.state_dict()),
+            ])
+            torch.save(state, os.path.join(args.outdir, f'model_state.pth'))
+            error = val_loss
 
         # Plot
-        plot_curve(args, plot_train_loss, plot_train_pitch_loss, plot_train_yaw_loss, plot_test_loss, plot_test_pitch_loss, plot_test_yaw_loss)
+        plot_curve(args, plot_train_loss, plot_train_pitch_loss, plot_train_yaw_loss, plot_val_loss, plot_val_pitch_loss, plot_val_yaw_loss)
 
 
 
