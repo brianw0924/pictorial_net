3cb296efd58b985132a0231591d4129791e2f243
brian <brianw0924@gmail.com>
2021-11-19 13:32:43 +0800

    add error distribution

--- inference_TEyeD.py
+++ inference_TEyeD.py
@@ -20,24 +20,71 @@ from collections import OrderedDict
 from tqdm import tqdm
 
 from utils import Set_seed, vec2angle, pitch_loss, yaw_loss
-from dataloader import TEyeDDataset
-from dataloader import get_loader_TEyeD
+from dataloader_v2 import get_loader_TEyeD
 from models.mynet import Mynet 
-from main import test
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, default="/home/brianw0924/hdd/processed_data")
+    parser.add_argument('--dataset', type=str, default="/home/brianw0924/hdd/TEyeD")
     parser.add_argument('--model_dir', type=str, default="./result")
     parser.add_argument('--seed', type=int, default=17)
-    parser.add_argument('--num_workers', type=int, default=7)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=8)
 
     args = parser.parse_args()
 
     assert os.path.exists(args.dataset)
 
     return args
+
+def inference(args, epoch, model, criterion, test_loader, device):
+    model.eval()
+
+    loss_list = []
+    pitch_loss_list = []
+    yaw_loss_list = []
+
+    num = 0
+    for step, (images, gazes) in enumerate(tqdm(test_loader)):
+        num+=gazes.shape[0]
+        with torch.no_grad():
+            outputs = model(images.to(device))
+
+        loss = criterion(outputs, vec2angle(gazes).to(device))
+        p_loss = pitch_loss(outputs, vec2angle(gazes).to(device))
+        y_loss = yaw_loss(outputs, vec2angle(gazes).to(device))
+
+        loss_list.append(loss.item())
+        pitch_loss_list.append(p_loss.item())
+        yaw_loss_list.append(y_loss.item())
+
+
+    loss_avg = sum(loss_list) / num
+    pitch_loss_avg = sum(pitch_loss_list) / num
+    yaw_loss_avg = sum(yaw_loss_list) / num
+    
+    tqdm.write( 'Test  | '
+                'Epoch {} | '
+                'Loss {:.4f} | '
+                'Pitch loss {:.2f} | '
+                'Yaw loss {:2f}'.format(
+                    epoch,
+                    loss_avg,
+                    pitch_loss_avg,
+                    yaw_loss_avg
+                ))
+
+    p_distribution = [0,0,0,0,0,0]
+    y_distribution = [0,0,0,0,0,0]
+
+    for i in range(6):
+        p_distribution[i]+= (torch.tensor(pitch_loss_list)<i).sum().item()
+        y_distribution[i]+= (torch.tensor(yaw_loss_list)<i).sum().item()
+
+    p_distribution = [ round(i/len(test_loader)*100) for i in p_distribution]
+    y_distribution = [ round(i/len(test_loader)*100) for i in y_distribution]
+    return p_distribution, y_distribution
+
 
 def main():
 
@@ -53,19 +100,23 @@ def main():
 
     # DATASET (TEyeD)
     print("Preparing data ...")
-    _, test_loader = get_loader_TEyeD(
-        args.dataset, args.batch_size, args.num_workers, True)
+    _, test_loader = get_loader_TEyeD(args)
 
     # MODEL & LOSS
     model = Mynet()
-    criterion = nn.MSELoss(reduction='mean') # actually, default is already 'mean'
+    criterion = nn.MSELoss(reduction='sum') # actually, default is already 'mean'
 
     # LOAD MODEL
-    model.load_state_dict(torch.load(os.path.join(args.model_dir, "UNet", "model_state_epoch20.pth"))['state_dict'])
+    model.load_state_dict(torch.load(os.path.join(args.model_dir, "UNet16_linear1024", "model_state.pth"))['state_dict'])
     model.to(device)
 
     # TESTING
-    test(0, model, criterion, test_loader, device)
+    P, Y = inference(args, 0, model, criterion, test_loader, device)
+    print("           pitch  |  yaw")
+    for i in range(6):
+        print(f'<={i} degree: {P[i]:3}%  |  {Y[i]}%')
+
+
 
 if __name__ == '__main__':
     main()
