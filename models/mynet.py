24aa3f0e84e2f3c59cee52ac1d87905be3fc67d2
brian <brianw0924@gmail.com>
2021-12-18 20:20:33 +0800

    rewrite mynet

--- models/mynet.py
+++ models/mynet.py
@@ -9,22 +9,48 @@ import torchvision.models as models
 from .unet import UNet
 
 
-class Mynet(nn.Module):
+class Gaze_Net(nn.Module):
     def __init__(self):
-        super(Mynet, self).__init__()
-        self.UNet = UNet(in_channels=1, out_channels=64, bilinear=True)
-        # self.resnet = models.resnet50(num_classes=2)
-        # self.resnet.conv1 = nn.Conv2d(64, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-       
-        self.vgg16_bn = models.vgg16_bn(num_classes=2)
-        self.vgg16_bn.features[0] = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)) 
+        super(Gaze_Net, self).__init__()
+        self.UNet = UNet(in_channels=1, out_channels=1, num=16, bilinear=True)
+        
+        self.vgg16_bn = models.vgg16_bn(pretrained=True)
+        self.vgg16_bn.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)) 
+        self.vgg16_bn.classifier[6] = nn.Linear(in_features=4096, out_features=2, bias=True)
 
     def forward(self, x):
         y = self.UNet(x)
-        # y = self.resnet(y)
+        y = self.vgg16_bn(y)
+        return y
+
+class Eye_Localization_Net(nn.Module):
+    def __init__(self):
+        super(Eye_Localization_Net, self).__init__()
+        self.UNet = UNet(in_channels=1, out_channels=1, num=16, bilinear=True)
+        
+        self.vgg16_bn = models.vgg16_bn(pretrained=True)
+        self.vgg16_bn.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)) 
+        self.vgg16_bn.classifier[6] = nn.Linear(in_features=4096, out_features=6, bias=True)
+
+    def forward(self, x):
+        y = self.UNet(x)
+        y = self.vgg16_bn(y)
+        return y
+
+class Lid_2point(nn.Module):
+    def __init__(self):
+        super(Lid_2point, self).__init__()
+        self.UNet = UNet(in_channels=1, out_channels=1, num=16, bilinear=True)
+        
+        self.vgg16_bn = models.vgg16_bn(pretrained=True)
+        self.vgg16_bn.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)) 
+        self.vgg16_bn.classifier[6] = nn.Linear(in_features=4096, out_features=4, bias=True)
+
+    def forward(self, x):
+        y = self.UNet(x)
         y = self.vgg16_bn(y)
         return y
 
 if __name__ == "__main__":
-    net = Mynet()
+    net = Gaze_Net()
     print(net)
