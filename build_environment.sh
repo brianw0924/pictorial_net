d4cd99a2ee0859f176111334104f34a35c72f805
brian <brianw0924@gmail.com>
2022-01-25 01:46:25 +0800

    script for building environment

--- build_environment.sh
+++ build_environment.sh
@@ -3,10 +3,11 @@ RED='\033[0;31m'
 # make sure argument exist
 if [ $# -eq 0 ]
   then
-    echo -e "${RED}No arguments supplied, please run: bash install.sh YOUR_ENV_NAME  "
+    echo -e "${RED}No arguments supplied, please run: source build_environment.sh YOUR_ENV_NAME  "
     exit -1
 fi
 
+# build environment & activate it
 conda create --name $1 python==3.8.5
 conda activate $1
 
@@ -17,5 +18,6 @@ if [ $CONDA_DEFAULT_ENV != $1 ]
     exit -1
 fi
 
+# install packages
 pip install -r requirements.txt
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=11.0 -c pytorch
