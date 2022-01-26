5557e75fd30249feb47339950ab33a188d8e10cd
brian <brianw0924@gmail.com>
2022-01-27 03:48:34 +0800

    download model

---
+++ download_model.sh
@@ -0,0 +1,3 @@
+wget 'https://www.dropbox.com/s/ige2v6d0u3w2wcv/gaze.pth?dl=1'
+wget 'https://www.dropbox.com/s/dh65bcefx5sn0bw/valid.pth?dl=1'
+wget 'https://www.dropbox.com/s/d44pdynkzab0ezo/pretrain.pth?dl=1'
